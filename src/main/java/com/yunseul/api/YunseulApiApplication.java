0d7670a1a3f2413a0101a3950d39d21309d53ed9
이인환(Inhwan Lee)/개발혁신팀/SKP <eenan@sk.com>
2017-08-13 15:56:36 +0900

    프로젝트 생성

---
+++ src/main/java/com/yunseul/api/YunseulApiApplication.java
@@ -0,0 +1,12 @@
+package com.yunseul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunseulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunseulApiApplication.class, args);
+	}
+}
